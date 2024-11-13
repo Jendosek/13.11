5a46b2e4cb1705f299d00747bc4e4ba415c51e77
Балакер Євгеній Максимович <131793662+Jendosek@users.noreply.github.com>
2024-11-13 17:40:13 +0200

    Add files via upload

---
+++ practical/task4/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Text;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main()
+        {
+
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+
+            Console.WriteLine("Введіть межі числового діапазону:");
+
+            int lowerBound = int.Parse(Console.ReadLine());
+            int upperBound = int.Parse(Console.ReadLine());
+
+            if (lowerBound > upperBound)
+            {
+                Console.WriteLine("Невірний діапазон. Нижня межа повинна бути меншою за верхню.");
+                return;
+            }
+
+            int a = 0, b = 1;
+
+            Console.WriteLine("Числа Фібоначчі в діапазоні:");
+
+            if (lowerBound <= a && a <= upperBound)
+            {
+                Console.Write(a + " ");
+            }
+
+            while (b <= upperBound)
+            {
+                if (b >= lowerBound)
+                {
+                    Console.Write(b + " ");
+                }
+
+                int next = a + b;
+                a = b;
+                b = next;
+            }
+            Console.WriteLine();
+        }
+    }
+}
