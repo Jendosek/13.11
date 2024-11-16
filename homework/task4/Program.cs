a0379855a0ea58f172eea0f93ef8b0c9feeb568e
Балакер Євгеній Максимович <131793662+Jendosek@users.noreply.github.com>
2024-11-16 14:41:45 +0200

    Add files via upload

---
+++ homework/task4/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("vvedit 6 chisel: ");
+            string input = Console.ReadLine();
+
+            if (input.Length != 6)
+            {
+                Console.WriteLine("mala chisel.");
+                return;
+            }
+
+            Console.Write("vvedit cherez probel kavo pamenat: ");
+
+            string[] positions = Console.ReadLine().Split();
+
+            int pos1 = int.Parse(positions[0]) - 1;
+            int pos2 = int.Parse(positions[1]) - 1;
+
+            char[] digits = input.ToCharArray();
+            char temp = digits[pos1];
+            digits[pos1] = digits[pos2];
+            digits[pos2] = temp;
+
+            Console.WriteLine(new string(digits));
+
+        }
+    }
+}
